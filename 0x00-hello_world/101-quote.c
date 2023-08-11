0933ec43b44ed6b2a97e68ff7a68b59e611b8907
Rockson101 <davidstanley331@gmail.com>
2023-08-11 06:32:40 -0700

    8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,10 @@
-#include <unistdio.h>
+#include <unistd.h>
 /**
  * main - A program that prints a line to the standard error
  * Return: 1 (Success)
  */
 int main(void)
 {
-fputs(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 57);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 57);
 return (1);
 }
