2a2c59cde18242a5e92ecdbf68746258a45264f4
Rockson101 <davidstanley331@gmail.com>
2023-08-10 19:50:53 -0700

    4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.hr>
+/**
+ * main - I am writing a progam to print a line that has function puts
+ * Return: 0
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
