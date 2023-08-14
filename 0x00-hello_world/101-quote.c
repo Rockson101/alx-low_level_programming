b16817975a939b3de67565b6b2a877ae2a9ed39c
Rockson101 <davidstanley331@gmail.com>
2023-08-14 16:13:40 -0700

    8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,8 +6,7 @@
  */
 int main(void)
 {
-        char *text = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-
-        write(2, text, strlen(text));
-        return (1);
+char *text = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(2, text, strlen(text));
+return (1);
 }
