c41aedc5b32116d037ca18e51562d5ff28e666f2
Rockson101 <davidstanley331@gmail.com>
2023-08-14 16:08:27 -0700

    8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,13 @@
+#include <string.h>
 #include <unistd.h>
 /**
- * main - A program that prints a line to the standard error
- * Return: 1 (Success)
+ * main - It's a program that prints a line to the standard error
+ * Return: 1
  */
 int main(void)
 {
-write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 57);
-return (1);
+        char *text = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+        write(2, text, strlen(text));
+        return (1);
 }
