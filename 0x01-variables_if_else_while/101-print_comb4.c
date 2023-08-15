b49103def7891279ea1c0fdb29c46025f88bae8a
Rockson101 <davidstanley331@gmail.com>
2023-08-14 22:16:18 -0700

    2

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -7,20 +7,23 @@
 int main(void)
 {
 	int n, m, l;
+
 	for (n = 48; n < 58; n++)
 	{
-		for (m =49; m < 58; m++)
+		for (m = 49; m < 58; m++)
 		{
-			for (1 = 50; l < 58; 1++)
+			for (l = 50; l < 58; l++)
 			{
 				if (1 >  m && m > n)
-				putchar(n);
-				putchar(m);
-				putchar(1);
-				if (n != 55 || m != 66)
 				{
-					putchgar (',');
-					putchar (' ');
+					putchar(n);
+					putchar(m);
+					putchar(1);
+					if (n != 55 || m != 56)
+					{
+						putchar (',');
+						putchar (' ');
+					}
 				}
 
 			}
